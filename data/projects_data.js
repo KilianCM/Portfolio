a1cffe32addd8fa0812f07bf9f1d35675894f531
kchamiot <chamiot.k@gmail.com>
2020-04-07 13:55:32 +0200

    Restructure images folder and remove skills percentage

--- data/projects_data.js
+++ data/projects_data.js
@@ -7,10 +7,10 @@
             "description" : "In the context of the second year university project, we have to develop an Android application for the company \"Espace Revêtements TSD\". This application permit to collect all the information of the ERP (customers, works, ...) to have all the information when the user is away from his office. He can call or text the customer directly from the app, send a mail, modify the information or create a new one.<br/><br/> It is developed with the framework Ionic that uses AngularJS and TypeScript.",
             "technos": ["Ionic", "TypeScript"],
             "images": [
-                "tsd_1.png",
-                "tsd_2.png",
-                "tsd_3.png",
-                "tsd_4.png"
+                "tsd/1.png",
+                "tsd/2.png",
+                "tsd/3.png",
+                "tsd/4.png"
             ]
         },
         "geoquizz":
@@ -20,11 +20,11 @@
             "description" : "During Android courses, we had to develop in team of 3 guys an Android application. We decided to design a quizz app based on the geolocation. The questions are about the city where you are (or choose one) and the answers are created with the help of GeoAPI. This is a free API made by the French government which provide demographical information. We used Google Places API to make autocompletion on the city research.",
             "technos": ["Android", "Java", "Figma"],
             "images": [
-                "geoquizz_1.png",
-                "geoquizz_2.png",
-                "geoquizz_3.png",
-                "geoquizz_4.png",
-                "geoquizz_5.png"
+                "geoquizz/1.png",
+                "geoquizz/2.png",
+                "geoquizz/3.png",
+                "geoquizz/4.png",
+                "geoquizz/5.png"
             ]
         },
         "mmf":
@@ -34,9 +34,9 @@
             "description" : "In order to manage my fuel consumption of my car, I developed an Ionic App. I insert data when I refuel my car and the app make statistics and graphs. Datas are inserted in an online database.",
             "technos": ["Ionic", "TypeScript"],
             "images": [
-                "mmf_1.jpg",
-                "mmf_2.jpg",
-                "mmf_3.jpg"
+                "mmf/1.jpg",
+                "mmf/2.jpg",
+                "mmf/3.jpg"
             ]
         },
         "audito":
@@ -46,9 +46,9 @@
             "description" : "Fictive website for managing a performance venue for administrators and clients with management of the venue's programming calendar, event details, online booking with Symfony, API Platform and VueJS.",
             "technos": ["Symfony", "API Platform", "VueJS"],
             "images": [
-                "audito_home.PNG",
-                "audito_event.PNG",
-                "audito_programmation.PNG"
+                "audito/home.PNG",
+                "audito/event.PNG",
+                "audito/programmation.PNG"
             ]
         },
         "portfolio":
@@ -67,9 +67,9 @@
             "description" : "Two months after the beginning of our studies, we had a special development week where we had to release a 2D game using SFML, a C++ graphic library. From the monday morning to the friday afternoon, we had to choose a style and a theme for our game and develop it. <br/><br/>Our game, called \"Tasmania Adventures\", is an endless runner game. We play the role of a platypus which should avoid rocks and floating woods on a river. At the beginning, the player has 3 lives and a bonus (a drilling machine). This bonus can be collect in the game, it is used to destroy obstacles, that can fill the entire river.",
             "technos": ["C++", "SFML"],
             "images": [
-                "ta_1.jpg",
-                "ta_2.jpg",
-                "ta_3.jpg"
+                "ta/1.jpg",
+                "ta/2.jpg",
+                "ta/3.jpg"
             ]
         }
     }
