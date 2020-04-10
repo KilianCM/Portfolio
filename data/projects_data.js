e014df2d5631185357916e1547125963ccb48016
kchamiot <chamiot.k@gmail.com>
2020-04-10 09:42:25 +0200

    Update projects data (wip)

--- data/projects_data.js
+++ data/projects_data.js
@@ -1,76 +1,107 @@
-
-   let projectsData = {
-        "tsd":
-        {
-            "name" : "Timono app",
-            "subname" : "Second year university project",  
-            "description" : "In the context of the second year university project, we have to develop an Android application for the company \"Espace Revêtements TSD\". This application permit to collect all the information of the ERP (customers, works, ...) to have all the information when the user is away from his office. He can call or text the customer directly from the app, send a mail, modify the information or create a new one.<br/><br/> It is developed with the framework Ionic that uses AngularJS and TypeScript.",
-            "technos": ["Ionic", "TypeScript"],
-            "images": [
-                "tsd/1.png",
-                "tsd/2.png",
-                "tsd/3.png",
-                "tsd/4.png"
-            ]
-        },
-        "geoquizz":
-        {
-            "name" : "GeoQuizz",
-            "subname" : "Android app", 
-            "description" : "During Android courses, we had to develop in team of 3 guys an Android application. We decided to design a quizz app based on the geolocation. The questions are about the city where you are (or choose one) and the answers are created with the help of GeoAPI. This is a free API made by the French government which provide demographical information. We used Google Places API to make autocompletion on the city research.",
-            "technos": ["Android", "Java", "Figma"],
-            "images": [
-                "geoquizz/1.png",
-                "geoquizz/2.png",
-                "geoquizz/3.png",
-                "geoquizz/4.png",
-                "geoquizz/5.png"
-            ]
-        },
-        "mmf":
-        {
-            "name" : "ManageMyFuel",
-            "subname": "Personal Ionic app", 
-            "description" : "In order to manage my fuel consumption of my car, I developed an Ionic App. I insert data when I refuel my car and the app make statistics and graphs. Datas are inserted in an online database.",
-            "technos": ["Ionic", "TypeScript"],
-            "images": [
-                "mmf/1.jpg",
-                "mmf/2.jpg",
-                "mmf/3.jpg"
-            ]
-        },
-        "audito":
-        {
-            "name" : "Audito",
-            "subname" : "University project", 
-            "description" : "Fictive website for managing a performance venue for administrators and clients with management of the venue's programming calendar, event details, online booking with Symfony, API Platform and VueJS.",
-            "technos": ["Symfony", "API Platform", "VueJS"],
-            "images": [
-                "audito/home.PNG",
-                "audito/event.PNG",
-                "audito/programmation.PNG"
-            ]
-        },
-        "portfolio":
-        {
-            "name" : "My portfolio",
-            "subname" : "The website you are visiting now",  
-            "description" : "This website has been released from scratch with HTML/CSS and native JavaScript. It presents my school career, my IT skills, my professional experiences and my school realisations but also my personal projects. I used <a href='http://robinparisi.github.io/tingle/'>TingleJS</a> to make beautiful modal windows and <a href='https://nickpiscitelli.github.io/Glider.js/'>Glider.js</a> to display correctly images of my projects.",
-            "technos": ["HTML", "CSS", "JS"],
-            "images": [
-            ]
-        },
-        "ta":
-        {
-            "name" : "Tasmania Adventures",
-            "subname" : "2D game",  
-            "description" : "Two months after the beginning of our studies, we had a special development week where we had to release a 2D game using SFML, a C++ graphic library. From the monday morning to the friday afternoon, we had to choose a style and a theme for our game and develop it. <br/><br/>Our game, called \"Tasmania Adventures\", is an endless runner game. We play the role of a platypus which should avoid rocks and floating woods on a river. At the beginning, the player has 3 lives and a bonus (a drilling machine). This bonus can be collect in the game, it is used to destroy obstacles, that can fill the entire river.",
-            "technos": ["C++", "SFML"],
-            "images": [
-                "ta/1.jpg",
-                "ta/2.jpg",
-                "ta/3.jpg"
-            ]
-        }
+let projectsData = {
+    "tsd": {
+        "name": "Timono app",
+        "subname": "Second year university project",
+        "description": "In the context of the second year university project, we have to develop an Android application for the company \"Espace Revêtements TSD\". This application permit to collect all the information of the ERP (customers, works, ...) to have all the information when the user is away from his office. He can call or text the customer directly from the app, send a mail, modify the information or create a new one.<br/><br/> It is developed with the framework Ionic that uses AngularJS and TypeScript.",
+        "technos": ["Ionic", "TypeScript"],
+        "images": [
+            "tsd/1.png",
+            "tsd/2.png",
+            "tsd/3.png",
+            "tsd/4.png"
+        ]
+    },
+    "geoquizz": {
+        "name": "GeoQuizz",
+        "subname": "Android app",
+        "description": "During Android courses, we had to develop in team of 3 guys an Android application. We decided to design a quizz app based on the geolocation. The questions are about the city where you are (or choose one) and the answers are created with the help of GeoAPI. This is a free API made by the French government which provide demographical information. We used Google Places API to make autocompletion on the city research.",
+        "technos": ["Android", "Java", "Figma"],
+        "images": [
+            "geoquizz/1.png",
+            "geoquizz/2.png",
+            "geoquizz/3.png",
+            "geoquizz/4.png",
+            "geoquizz/5.png"
+        ]
+    },
+    "mmf": {
+        "name": "ManageMyFuel",
+        "subname": "Personal Ionic app",
+        "description": "In order to manage my fuel consumption of my car, I developed an Ionic App. I insert data when I refuel my car and the app make statistics and graphs. Datas are inserted in an online database.",
+        "technos": ["Ionic", "TypeScript"],
+        "images": [
+            "mmf/1.jpg",
+            "mmf/2.jpg",
+            "mmf/3.jpg"
+        ]
+    },
+    "audito": {
+        "name": "Audito",
+        "subname": "University project",
+        "description": "Fictive website for managing a performance venue for administrators and clients with management of the venue's programming calendar, event details, online booking with Symfony, API Platform and VueJS.",
+        "technos": ["Symfony", "API Platform", "VueJS"],
+        "images": [
+            "audito/home.PNG",
+            "audito/event.PNG",
+            "audito/programmation.PNG"
+        ]
+    },
+    "portfolio": {
+        "name": "My portfolio",
+        "subname": "The website you are visiting now",
+        "description": "This website has been released from scratch with HTML/CSS and native JavaScript. It presents my school career, my IT skills, my professional experiences and my school realisations but also my personal projects. I used <a href='http://robinparisi.github.io/tingle/'>TingleJS</a> to make beautiful modal windows and <a href='https://nickpiscitelli.github.io/Glider.js/'>Glider.js</a> to display correctly images of my projects.",
+        "technos": ["HTML", "CSS", "JS"],
+        "images": []
+    },
+    "ta": {
+        "name": "Tasmania Adventures",
+        "subname": "2D game",
+        "description": "Two months after the beginning of our studies, we had a special development week where we had to release a 2D game using SFML, a C++ graphic library. From the monday morning to the friday afternoon, we had to choose a style and a theme for our game and develop it. <br/><br/>Our game, called \"Tasmania Adventures\", is an endless runner game. We play the role of a platypus which should avoid rocks and floating woods on a river. At the beginning, the player has 3 lives and a bonus (a drilling machine). This bonus can be collect in the game, it is used to destroy obstacles, that can fill the entire river.",
+        "technos": ["C++", "SFML"],
+        "images": [
+            "ta/1.jpg",
+            "ta/2.jpg",
+            "ta/3.jpg"
+        ]
+    },
+    "movies": {
+        "name": "Movies Explorer",
+        "subname": "iOS app",
+        "description": "",
+        "technos": ["Swift", "TheMovieDB API"],
+        "images": [
+            "movies-explorer/1.png",
+            "movies-explorer/2.png",
+            "movies-explorer/3.png",
+            "movies-explorer/4.png"
+        ]
+    },
+    "spaceup": {
+        "name": "SpaceUP",
+        "subname": "University project",
+        "description": "",
+        "technos": ["Unity", "C#"],
+        "images": []
+    },
+    "hulahop": {
+        "name": "HulaHop",
+        "subname": "University project",
+        "description": "",
+        "technos": ["Symfony", "Twig", "Photon API", "BoardGamesAtlas API"],
+        "images": []
+    },
+    "wrcpredict": {
+        "name": "WRC Predict",
+        "subname": "Personal future project",
+        "description": "",
+        "technos": ["Figma"],
+        "images": [
+            "wrcpredict/1.png",
+            "wrcpredict/2.png",
+            "wrcpredict/3.png",
+            "wrcpredict/4.png",
+            "wrcpredict/5.png",
+        ]
     }
+};
 
